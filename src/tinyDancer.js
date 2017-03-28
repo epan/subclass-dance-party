d0f65840595d377cd8ca8158506dd3eb97ebd568
MKS-Student <mks.student@hackreactor.com>
2017-03-28 13:35:46 -0700

    Add second new dancer

---
+++ src/tinyDancer.js
@@ -0,0 +1,11 @@
+var MakeTinyDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('tiny');
+};
+
+MakeTinyDancer.prototype = Object.create(MakeDancer.prototype);
+MakeTinyDancer.prototype.constructor = MakeTinyDancer;
+MakeTinyDancer.prototype.step = function () {
+  MakeDancer.prototype.step.call(this);
+  this.$node.toggle();
+};
