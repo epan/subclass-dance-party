9057762cf4bf6d22d39463aadc8f49314a8ff4c5
MKS-Student <mks.student@hackreactor.com>
2017-03-27 19:50:50 -0700

    Pass first test for blinkyDancer, refactor to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,50 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
+var MakeDancer = function(top, left, timeBetweenSteps) {
 
-  var dancer = {};
+  // var dancer = {};
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+  this.styleSettings = {
+    top: this.top,
+    left: this.left
   };
-  dancer.step();
+  this.$node = $('<span class="dancer"></span>').css(this.styleSettings);
 
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
+  // dancer.step = function() {
+  //   // the basic dancer doesn't do anything interesting at all on each step,
+  //   // it just schedules the next step
+  //   setTimeout(dancer.step, timeBetweenSteps);
+  // };
+
+  // dancer.oldStep = function() {
+  //   // the basic dancer doesn't do anything interesting at all on each step,
+  //   // it just schedules the next step
+  //   setTimeout(dancer.step, timeBetweenSteps);
+  // };
+  // dancer.oldStep();
+
+  // dancer.setPosition = function(top, left) {
+  //   // Use css top and left properties to position our <span> tag
+  //   // where it belongs on the page. See http://api.jquery.com/css/
+  //   //
+  //   var styleSettings = {
+  //     top: top,
+  //     left: left
+  //   };
+  //   dancer.$node.css(styleSettings);
+  // };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  //dancer.setPosition(top, left);
 
-  return dancer;
+  // return dancer;
+};
+
+MakeDancer.prototype.oldStep = function () {
+  setTimeout(step.call(this), this.timeBetweenSteps);
+};
+
+MakeDancer.prototype.step = function () {
+  setTimeout(this.step, this.timeBetweenSteps);
 };
